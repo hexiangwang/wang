c17dd49366cbf721d9b8b53b834e142c51a57676
hexiangwang <hexiangwang88@163.com>
2019-06-03 22:38:11 +0800

    add files

--- spring-cloud-demo/service-hi/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
+++ spring-cloud-demo/service-hi/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
@@ -1,11 +1,15 @@
 package com.forezp.servicehi;
 
 import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +21,9 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @RestController
 public class ServiceHiApplication {
 
@@ -31,9 +38,12 @@ public class ServiceHiApplication {
     String port;
 
     @RequestMapping("/home")
+    //@HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
         return "hi " + name + " ,i am from port:" + port;
     }
+
+
 
     private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
 
@@ -46,7 +56,8 @@ public class ServiceHiApplication {
     }
 
     @RequestMapping("/hi")
-    public String callHome(){
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String callHome(String name){
         LOG.log(Level.INFO, "calling trace service-hi  ");
         return restTemplate.getForObject("http://localhost:8989/hi", String.class);
     }
@@ -62,5 +73,12 @@ public class ServiceHiApplication {
     public Sampler defaultSampler() {
         return Sampler.ALWAYS_SAMPLE;
     }
+
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+
+
 
 }
---
+++ spring-cloud-demo/service-turbine/src/main/java/com/wang/serviceturbine/ServiceTurbineApplication.java
@@ -0,0 +1,25 @@
+package com.wang.serviceturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableTurbine
+public class ServiceTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceTurbineApplication.class, args);
+    }
+
+}
